c9aebc8bf3460687ea945fd3d7b971b6e05559bf
Crowdbotics <team@crowdbotics.com>
2021-09-09 12:10:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lunar_war_30416Identifier",
+  url: "https://lunar_war_30416Identifier.botics.co",
+  api: "https://lunar_war_30416Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
